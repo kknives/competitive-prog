52de7889e59607b13db51e5c5351cb55da31b1bc
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-03-23 09:16:47 +0530

    cses.fi: Counting Rooms - find neighbours of point

--- cses.fi/counting_rooms.cpp
+++ cses.fi/counting_rooms.cpp
@@ -13,11 +13,18 @@ main() -> int
       std::cin >> plan[i][j];
   }
 
+  auto nearest_edges = [](const auto p) {
+    const auto [x, y] = p;
+    return std::array<std::tuple<int, int>, 4>{ std::make_pair(x, y + 1),
+                                                std::make_pair(x, y - 1),
+                                                std::make_pair(x - 1, y),
+                                                std::make_pair(x + 1, y) };
+  };
   std::deque<std::tuple<int, int>> upcoming{ std::make_pair(0, 0) };
   while (!upcoming.empty()) {
-    auto [x, y] = upcoming.front();
-
-    if (plan[x][y] == '.') {
+    auto point = upcoming.front();
+    auto neighb = nearest_edges(point);
+    for (auto t : neighb) {
     }
   }
 }
