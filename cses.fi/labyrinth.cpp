2582d773b794ae882882511fcddec10d748a6a04
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-04-15 09:08:55 +0530

    cses.fi: Labyrinth, use struct instead of array
    `std::deque` doesn't accept int[] as the element type, due to the Copy
    requirements. But a simple POD struct works

--- cses.fi/labyrinth.cpp
+++ cses.fi/labyrinth.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
 
+struct Coord
+{
+  int i, j;
+};
 int
 main()
 {
   int n, m;
-  int bgn[2] = { -1, -1 }, dst[2] = { -1, -1 };
+  auto bgn = Coord{ -1, -1 }, dst = Coord{ -1, -1 };
   std::cin >> n >> m;
   std::vector<std::string> plan(n, "");
   for (int i = 0; i < n; i++) {
     std::cin >> plan[i];
-    if (bgn[0] == -1) {
+    if (bgn.i == -1) {
       if (auto bgn_pot = plan[i].find('A'); bgn_pot != std::string::npos) {
-        bgn[0] = i;
-        bgn[1] = bgn_pot;
+        bgn = { i, static_cast<int>(bgn_pot) };
       }
     }
-    if (dst[0] == -1) {
+    if (dst.i == -1) {
       if (auto dst_pot = plan[i].find('B'); dst_pot != std::string::npos) {
-        dst[0] = i;
-        dst[1] = dst_pot;
+        dst = { i, static_cast<int>(dst_pot) };
       }
     }
   }
