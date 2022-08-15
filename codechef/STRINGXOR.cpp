62bf0c912e3ca6e375d3fe1cc74e8a0aa52f1a3a
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-08-15 14:02:35 +0530

    codechef: Implement BT for STRINGXOR
    Still doesn't work. Way too slow and there's a segfault somewhere.

--- codechef/STRINGXOR.cpp
+++ codechef/STRINGXOR.cpp
@@ -1,18 +1,70 @@
 #include <bits/stdc++.h>
 
 std::string results[] = { "NO", "YES" };
+
 int
-transform(int from, int to)
+bt(const int len, char* init, char* fin, int w)
 {
-  switch (from) {
+  if (w > len)
+    return true;
+  if (init[w] == fin[w])
+    return bt(len, init, fin, w + 1);
+  else {
+    char temp_b, temp_f;
+    int chg;
+    int back_res = false;
+    if ((w > 0) && (((init[w] == init[w - 1]) && (fin[w] == fin[w - 1])) ||
+                    (init[w] != init[w - 1]))) {
+      temp_b = init[w - 1], temp_f = init[w];
+      chg = (init[w - 1] - '0') ^ (init[w] - '0');
+      init[w] = chg + '0';
+      init[w - 1] = chg + '0';
+      back_res = bt(len, init, fin, w - 1);
+      init[w - 1] = temp_b;
+      init[w] = temp_f;
+    }
+    if (back_res)
+      return back_res;
+
+    int forw_res = false;
+    if ((w < len - 1) &&
+        (((init[w] == init[w + 1]) && (fin[w] == fin[w + 1])) ||
+         (init[w] != init[w + 1]))) {
+      temp_b = init[w];
+      temp_f = init[w + 1];
+      chg = (init[w] - '0') ^ (init[w + 1] - '0');
+      init[w] = chg + '0';
+      init[w + 1] = chg + '0';
+      forw_res = bt(len, init, fin, w);
+      init[w] = temp_b;
+      init[w + 1] = temp_f;
+    }
+
+    return back_res || forw_res;
+  }
+}
+int
+transform(const int len, char* init, char* fin)
+{
+  if (len == 1)
+    return init[0] == fin[0];
+  if (len <= 0)
+    return 0;
+  int init_pair = (init[0] - '0') + (init[1] - '0' << 1);
+  int fin_pair = (fin[0] - '0') + (fin[1] - '0' << 1);
+
+  switch (init_pair) {
     case 0b00:
-      return to == 0b00;
+      return ((fin_pair == 0b00)) && transform(len - 2, init + 2, fin + 2);
     case 0b01:
-      return ((to == 0b00) || (to == 0b11) || (to == 0b01));
+      return ((fin_pair == 0b00) || (fin_pair == 0b11) || (fin_pair == 0b01)) &&
+             transform(len - 2, init + 2, fin + 2);
     case 0b10:
-      return ((to == 0b10) || (to == 0b00) || (to == 0b11));
+      return ((fin_pair == 0b10) || (fin_pair == 0b00) || (fin_pair == 0b11)) &&
+             transform(len - 2, init + 2, fin + 2);
     case 0b11:
-      return ((to == 0b00) || (to == 0b11));
+      return ((fin_pair == 0b00) || (fin_pair == 0b11)) &&
+             transform(len - 2, init + 2, fin + 2);
     default:
       return false;
   }
@@ -24,35 +76,9 @@ main()
   std::cin >> tc;
   while (tc--) {
     int len;
-    std::string init{}, fin{};
-    std::cin >> len >> init >> fin;
-    int res = 0;
-    for (int i = 0; i < len; i++) {
-      if (init[i] == fin[i])
-        res = 1;
-      else if (i + 1 == len) {
-        int bac_init_pair = (init[i - 1] - '0') + (init[i] - '0' << 1);
-        int bac_fin_pair = (fin[i - 1] - '0') + (fin[i] - '0' << 1);
-        res = transform(bac_init_pair, bac_fin_pair);
-      }
-
-      else if (i == 0) {
-        int for_fin_pair = (fin[i + 1] - '0') + (fin[i] - '0' << 1);
-        int for_init_pair = (init[i + 1] - '0') + (init[i] - '0' << 1);
-        res = transform(for_init_pair, for_fin_pair);
-      } else {
-        int for_init_pair = (init[i + 1] - '0') + (init[i] - '0' << 1);
-        int bac_init_pair = (init[i] - '0') + (init[i - 1] - '0' << 1);
-        int for_fin_pair = (fin[i + 1] - '0') + (fin[i] - '0' << 1);
-        int bac_fin_pair = (fin[i] - '0') + (fin[i - 1] - '0' << 1);
-
-        res = transform(for_init_pair, for_fin_pair) ||
-              transform(bac_init_pair, bac_fin_pair);
-      }
-      if (res == 0)
-        break;
-    }
-    std::cout << results[res] << "\n";
+    char *init, *fin;
+    scanf("%d\n%ms\n%ms", &len, &init, &fin);
+    std::cout << results[bt(len, init, fin, 0)] << "\n";
   }
   return 0;
 }
